1ecfa65f60013a0483d24387b1ed0a0e2b2c85c3
DevLeeYG <hi33741@naver.com>
2022-07-15 17:01:56 +0900

    '기본셋팅'

---
+++ src/main/java/com/springboot/jpa/Worksapce2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Worksapce2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Worksapce2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -0,0 +1,31 @@
+package com.springboot.jpa.data.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name ="product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+
+    @Column(nullable =false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updateAt;
+
+
+
+}
